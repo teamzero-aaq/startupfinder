4423700af76c3b0024e85cd0cc596f864ae7634f
hgokhru101 <54789722+hgokhru101@users.noreply.github.com>
2020-02-02 12:13:33 +0530

    Add files via upload

---
+++ fintech/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from fintech.models import user_profile11,add_idea11,register_user11,register_investor11,watchlist1
+admin.site.register(user_profile11)
+admin.site.register(add_idea11)
+admin.site.register(register_user11)
+admin.site.register(register_investor11)
+admin.site.register(watchlist1)
---
+++ fintech/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FintechConfig(AppConfig):
+    name = 'fintech'
